35da71b4a2e10516721a22a6f1aeeaab91d3c9a8
D4T4PL4Y3R <154893947+D4T4PL4Y3R@users.noreply.github.com>
2023-12-29 09:59:46 +0200

    Create playRound function

--- script.js
+++ script.js
@@ -4,3 +4,23 @@ function getComputerChoice() {
     let choice = choices[Math.floor(Math.random() * choices.length)]
     return choice;  
 }
+
+function playRound() {
+    let computerChoice = getComputerChoice()
+    let userChoice = prompt("What shall you choose?");
+    userChoice = userChoice.charAt(0).toUpperCase() + userChoice.slice(1).toLowerCase()
+
+    if (userChoice == "Rock" || userChoice == "Paper" || userChoice == "Scissors") {
+        if (userChoice == computerChoice) {
+            console.log("It's a tie!")
+        } else if (userChoice == "Rock" && computerChoice == "Scissors" || userChoice == "Paper" && computerChoice == "Rock" || userChoice == "Scissors" && computerChoice == "Paper") {
+            console.log("You win!")
+        } else {
+            console.log("You lost!")
+        }
+    }   else {
+        playRound()
+    }
+
+    
+}
