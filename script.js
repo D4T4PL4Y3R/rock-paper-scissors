51d5191dfb63ce5057c72f8d61044afd80fbeed9
D4T4PL4Y3R <154893947+D4T4PL4Y3R@users.noreply.github.com>
2023-12-29 10:09:23 +0200

    Add game function with scorekeeping

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const choices = ["Rock", "Paper", "Scissors"];
+let playerScore = 0;
+let computerScore = 0;
 
 function getComputerChoice() {
     let choice = choices[Math.floor(Math.random() * choices.length)]
@@ -15,12 +17,31 @@ function playRound() {
             console.log("It's a tie!")
         } else if (userChoice == "Rock" && computerChoice == "Scissors" || userChoice == "Paper" && computerChoice == "Rock" || userChoice == "Scissors" && computerChoice == "Paper") {
             console.log("You win!")
+            playerScore++;
         } else {
             console.log("You lost!")
+            computerScore++;
         }
     }   else {
         playRound()
     }
 
     
+}
+
+function game(n) {
+    n = 5;
+    for (let i = 0; i < n; i++) {
+        playRound()
+    } 
+    if (playerScore == computerScore) {
+        console.log("Game over! It's a tie")
+        console.log(`Final score is User: ${playerScore}  Computer: ${computerScore}`)
+    } else if (playerScore > computerScore) {
+        console.log("Game over! You have won!")
+        console.log(`Final score is User: ${playerScore}  Computer: ${computerScore}`)
+    } else {
+        console.log("Game over! Computer has beaten you!")
+        console.log(`Final score is User: ${playerScore}  Computer: ${computerScore}`)
+    }
 }
